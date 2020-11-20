5ea79ec3c5373f3df002058f8e267d4ff64fbde8
Hans <twd2206@gmail.com>
2020-11-20 15:03:33 -0500

    fixed

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ const env = require('./env.json')
 module.exports = {
     basePath: env.app.base_path,
     assetPrefix: env.app.domain,
-    distDir: 'build'
+    distDir: 'build',
+    publicRuntimeConfig: {
+        staticFolder: env.app.domain
+    }
 }
