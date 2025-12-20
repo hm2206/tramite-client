59fd42287352e2bb274e723c9f0a0199296f10d1
Hans <twd2206@gmail.com>
2025-12-20 07:53:27 -0500

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || "",
   distDir: ".next",
 };
